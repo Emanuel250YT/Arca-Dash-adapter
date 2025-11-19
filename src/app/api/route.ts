057ed7cc146a671cd06afc8b7cba5f795e56b8c3
Emanuel250YT <emanuel250gameryt@gmail.com>
2025-11-19 16:30:27 -0300

    refactor: streamline POST request handling by removing CLI token validation and simplifying video processing logic

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -21,15 +21,6 @@ for (const dir of [contentDir, dashDir, generalDir, tempPath]) {
 export async function POST(req: NextRequest): Promise<NextResponse> {
 
 
-  const session = await getSessionInfo()
-  const cliTokenHeader = req.headers.get('x-cli-token');
-
-  if ((!cliTokenHeader || cliTokenHeader != process.env.CLI_TOKEN) && (!session || session.user.role != "admin")) return new APIResponse({
-    body: null,
-    code: APICodes[401],
-    message: APIMessages.UnAuthorized,
-    status: APIStatus.Unauthorized
-  }).response()
 
 
   const formData = await req.formData();
@@ -59,115 +50,98 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
 
 
 
-  if (cliUpload && cliRoute) {
-    const cliPath = path.join(contentDir, cliRoute);
-    const partialDir = path.dirname(cliPath);
+  // if (cliUpload && cliRoute) {
+  //   const cliPath = path.join(contentDir, cliRoute);
+  //   const partialDir = path.dirname(cliPath);
 
-    if (partialDir && !fs.existsSync(partialDir)) {
-      fs.mkdirSync(partialDir, { recursive: true });
-    }
+  //   if (partialDir && !fs.existsSync(partialDir)) {
+  //     fs.mkdirSync(partialDir, { recursive: true });
+  //   }
 
-    fs.writeFileSync(cliPath, buffer);
+  //   fs.writeFileSync(cliPath, buffer);
 
-    const fileData = await CREATE_FILEDATA({
-      uuid: uuid,
-      file: `/api/v1/content/${cliRoute}`,
-      name: fileName,
-      size: buffer.length
-    })
+  //   const fileData = await CREATE_FILEDATA({
+  //     uuid: uuid,
+  //     file: `/api/v1/content/${cliRoute}`,
+  //     name: fileName,
+  //     size: buffer.length
+  //   })
 
-    return new APIResponse({
-      body: fileData,
-      code: APICodes[200],
-      message: APIMessages.OK,
-      status: APIStatus.Success
-    }).response();
+  //   return new APIResponse({
+  //     body: fileData,
+  //     code: APICodes[200],
+  //     message: APIMessages.OK,
+  //     status: APIStatus.Success
+  //   }).response();
 
-  }
+  // }
 
-  if (isVideo) {
-    const tempFilePath = path.join(tempPath, `${uuid}.${ext}`);
-    fs.writeFileSync(tempFilePath, buffer);
-    const outputDir = path.join(dashDir, uuid);
-    if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
+  const tempFilePath = path.join(tempPath, `${uuid}.${ext}`);
+  fs.writeFileSync(tempFilePath, buffer);
+  const outputDir = path.join(dashDir, uuid);
+  if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
 
-    const outputManifestPath = path.join(outputDir, 'index.mpd');
+  const outputManifestPath = path.join(outputDir, 'index.mpd');
 
-    testRead(tempFilePath);
-    return new Promise((resolve) => {
-      ffmpeg(tempFilePath)
-        .outputOptions([
-          '-profile:v', 'main',
-          '-keyint_min', '48',
-          '-g', '48',
-          '-sc_threshold', '0',
-          '-b_strategy', '0',
-          "-vf", "scale=1280:720",
-          '-bf', '1',
-          '-b:v', '1000k',
-          '-c:a', 'aac',
-          '-b:a', '128k',
-          '-map', '0:v:0',
-          '-map', '0:a:0?',
-          '-use_timeline', '1',
-          '-use_template', '1',
-          `-init_seg_name`, process.platform === 'win32' ? path.join(outputDir, 'init-$RepresentationID$.mp4') : 'init-$RepresentationID$.mp4',
-          `-media_seg_name`, process.platform === 'win32' ? path.join(outputDir, 'chunk-$RepresentationID$-$Number%05d$.m4s') : 'chunk-$RepresentationID$-$Number%05d$.m4s',
-          '-seg_duration', '4'
-        ])
-        .format('dash')
+  testRead(tempFilePath);
+  
+  return new Promise((resolve) => {
+    ffmpeg(tempFilePath)
+      .outputOptions([
+        '-profile:v', 'main',
+        '-keyint_min', '48',
+        '-g', '48',
+        '-sc_threshold', '0',
+        '-b_strategy', '0',
+        "-vf", "scale=1280:720",
+        '-bf', '1',
+        '-b:v', '1000k',
+        '-c:a', 'aac',
+        '-b:a', '128k',
+        '-map', '0:v:0',
+        '-map', '0:a:0?',
+        '-use_timeline', '1',
+        '-use_template', '1',
+        `-init_seg_name`, process.platform === 'win32' ? path.join(outputDir, 'init-$RepresentationID$.mp4') : 'init-$RepresentationID$.mp4',
+        `-media_seg_name`, process.platform === 'win32' ? path.join(outputDir, 'chunk-$RepresentationID$-$Number%05d$.m4s') : 'chunk-$RepresentationID$-$Number%05d$.m4s',
+        '-seg_duration', '4'
+      ])
+      .format('dash')
 
-        .output(outputManifestPath)
-        .on('end', async () => {
-          fs.unlinkSync(tempFilePath);
-          const fileData = await CREATE_FILEDATA({
-            uuid: uuid,
-            file: `/api/v1/content/dash/${uuid}/index.mpd`,
-            name: fileName,
-          })
-
-          resolve(new APIResponse({
-            body: fileData,
-            code: APICodes[200],
-            message: APIMessages.OK,
-            status: APIStatus.Success
-          }).response());
-
-          clearRoute(outputManifestPath)
+      .output(outputManifestPath)
+      .on('end', async () => {
+        fs.unlinkSync(tempFilePath);
+        const fileData = await CREATE_FILEDATA({
+          uuid: uuid,
+          file: `/api/v1/content/dash/${uuid}/index.mpd`,
+          name: fileName,
         })
-        .on('stderr', (line) => {
-          console.error('FFmpeg stderr:', line);
-        })
-        .on('error', (err) => {
-          console.error('Error during video processing:', err);
-          resolve(new APIResponse({
-            body: {},
-            code: APICodes[500],
-            message: APIMessages.InternalServerError,
-            status: APIStatus.InternalServerError
-          }).response());
-        })
-        .run();
-    });
 
-  } else {
-    const savePath = path.join(generalDir, `${uuid}.${ext}`);
-    fs.writeFileSync(savePath, buffer);
+        resolve(new APIResponse({
+          body: fileData,
+          code: APICodes[200],
+          message: APIMessages.OK,
+          status: APIStatus.Success
+        }).response());
 
-    const fileData = await CREATE_FILEDATA({
-      uuid: uuid,
-      file: `/api/v1/content/general/${uuid}.${ext}`,
-      name: fileName,
-      size: buffer.length
-    })
+        clearRoute(outputManifestPath)
+      })
+      .on('stderr', (line) => {
+        console.error('FFmpeg stderr:', line);
+      })
+      .on('error', (err) => {
+        console.error('Error during video processing:', err);
+        resolve(new APIResponse({
+          body: {},
+          code: APICodes[500],
+          message: APIMessages.InternalServerError,
+          status: APIStatus.InternalServerError
+        }).response());
+      })
+      .run();
+  });
 
-    return new APIResponse({
-      body: fileData,
-      code: APICodes[200],
-      message: APIMessages.OK,
-      status: APIStatus.Success
-    }).response()
-  }
+
 }
 
 
